45ed1c5baae42335c3d52c566a5b9d0f68d317c1
Fernando Santos <111710394+FerdSantos@users.noreply.github.com>
2022-08-28 17:39:26 +0100

    Update list_word.py

--- list_word.py
+++ list_word.py
@@ -237,6 +237,6 @@ word = ["aback", "abaft", "abandoned", "abashed", "aberrant", "abhorrent", "abid
           "young", "youthful", "yummy", "zany", "zealous", "zebra", "zephyr", "zesty", "zinc", "zip", "zipper", "zippy",
           "zonked", "zoo", "zoom"]
 letter = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",\
-          "t", "u", "v", "w", "x", "y", "z"]
+          "t", "u", "v", "w", "x", "y", "z", "-"]
 
 
