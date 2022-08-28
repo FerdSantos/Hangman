97172e2d03901b53befaf122cfccc96258e236a6
Fernando Santos <111710394+FerdSantos@users.noreply.github.com>
2022-08-28 17:37:19 +0100

    Update Hangman.py

--- Hangman.py
+++ Hangman.py
@@ -5,10 +5,18 @@ five_or_less = list(filter(lambda x: len(x) <= 5, word))
 six_to_nine = list(filter(lambda x: len(x) >= 6 and len(x) < 9, word))
 nine_or_more = list(filter(lambda x: len(x) >= 9, word))
 
+level = ()
 lives = 0
 game_word = []
 
-level = int(input("What level you want to play? (1 = easy ; 2 = medium ; 3= hard): "))
+while True:
+    level = input("What level you want to play? (1 = easy ; 2 = medium ; 3= hard): ")
+    if level == "1" or level == "2" or level == "3":
+        level = int(level)
+        break
+    else:
+        print("\n**Please answer with either 1, 2 or 3**\n")
+
 
 if level == 1:
     game_word = random.choice(five_or_less)
@@ -35,40 +43,43 @@ def indices(game_word, guess):
 
 while True:
     guess = input("Guess a letter: ")
-    if guess not in wrong_guess:
-        if guess not in right_guess:
-            if guess in game_word:
-                if game_word.count(guess) > 1:
-                    position = (indices(game_word, guess))
-                    for i in range (game_word.count(guess)):
-                        blank[position[i]] = guess
+    if guess in letter:
+        if guess not in wrong_guess:
+            if guess not in right_guess:
+                if guess in game_word:
+                    if game_word.count(guess) > 1:
+                        position = (indices(game_word, guess))
+                        for i in range (game_word.count(guess)):
+                            blank[position[i]] = guess
+                            right_guess.append(guess)
+                        print("\n",''.join(blank).upper(), "\n")
+                    else:
+                        position = (indices(game_word, guess))
+                        blank[position[0]] = guess
                         right_guess.append(guess)
-                    print(''.join(blank).upper())
+                        print("\n",''.join(blank).upper(), "\n")
                 else:
-                    position = (indices(game_word, guess))
-                    blank[position[0]] = guess
-                    right_guess.append(guess)
+                    print("\n *OH, WRONG LETTER*")
+                    lives = lives - 1
+                    print("\n Lives: ", lives, "\n")
                     print(''.join(blank).upper())
+                    wrong_guess.append(guess)
+                    print("\n Wrong guesses: " , wrong_guess, "\n")
+
+
+                if lives == 0:
+                    print("\n **I'M SORRY YOU LOST** \n")
+                    print("The right word was: " , game_word)
+                    break
+                if "_" not in blank:
+                   print("\n **CONGRATULATIONS! YOU WON!**")
+                   break
             else:
-                print("\n *OH, WRONG LETTER*")
-                lives = lives - 1
-                print("\n Lives: ", lives)
-                print(''.join(blank).upper())
-                wrong_guess.append(guess)
-                print("\n Wrong guesses: " , wrong_guess, "\n")
-
-
-            if lives == 0:
-                print("\n **I'M SORRY YOU LOST** \n")
-                print("The right word was: " , game_word)
-                break
-            if "_" not in blank:
-               print("\n **CONGRATULATIONS! YOU WON!**")
-               break
+                print("\nYou already tried this letter \n")
         else:
             print("\nYou already tried this letter \n")
     else:
-        print("\nYou already tried this letter \n")
+        print("\n **Please, type letters in lower case or '-'** ")
 
 
 
